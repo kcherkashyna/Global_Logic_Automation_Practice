737c0777e70ec087d0f64cd8a06cff55c6e80822
antonvarchenko <80907733+antonvarchenko@users.noreply.github.com>
2021-03-25 12:24:58 +0200

    Update AuthenticationPage.java

--- src/main/java/pages/AuthenticationPage.java
+++ src/main/java/pages/AuthenticationPage.java
@@ -1,14 +1,19 @@
 package pages;
 
 import static com.codeborne.selenide.Selenide.$;
-
+/*
+Need to add action package with classes that will contain action methods
+Page classes shall contain getters for SelenideElements and ElementCollection
+*/
 public class AuthenticationPage {
 
+    //Method parameters should be always final
     public void enterEmail(String email) {
         $("#email_create").setValue(email);
     }
 
     public void submitEmail() {
+        //Always check is element visible, present,etc before interact with it
         $("#SubmitCreate").click();
     }
 
