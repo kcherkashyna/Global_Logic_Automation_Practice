3f1ecba1297b0116125409ddd69aa71611f49e97
antonvarchenko <80907733+antonvarchenko@users.noreply.github.com>
2021-03-25 12:27:09 +0200

    Update MyAccountPage.java

--- src/main/java/pages/MyAccountPage.java
+++ src/main/java/pages/MyAccountPage.java
@@ -8,10 +8,11 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class MyAccountPage {
 
+    //Is good to have matcher metod in action class too
     public void checkAccountIsCreated() {
         $(".page-heading").shouldHave(text("MY ACCOUNT"));
     }
-
+    // use $x() for xpath and  think how you can avoid .get(0) 
     public void checkFirstAndLastName(String firstAndLastName) {
         $$(By.xpath("//*[@rel='nofollow']/span")).get(0).shouldHave(text(firstAndLastName));
     }
